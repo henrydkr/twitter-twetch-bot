f2ae286767a2e2785233315b17df159456f56bcb
henrydkr <henrydkr@gmail.com>
2020-02-09 21:24:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,24 +19,32 @@ def twetch(data):
 
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key=""
-consumer_secret=""
+consumer_key="l15bTjR7RMGbl8mLDt9qFWP7k"
+consumer_secret="WwCtEI2Y9tZOUgxNWL3mk4Yx8TZmdigrVFM5qGio9XaqdxEGHX"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token=""
-access_token_secret=""
+access_token="226707500-eidOyVv6klvDCscBQFlhqFZqmvRWoJYb9RwAMBoC"
+access_token_secret="vk3m8Rlz0CeLrdfPsKJpAjUgAUa4UkIxsy7EjuFI8wZeJ"
 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
-        
-        twetch(status.text)
+        if (status.user.screen_name=='realDonaldTrump'):
+            
+            
+            if hasattr(status, 'retweeted_status'):
+                print('retweet')
+            else:
+                tweet = status.text
+                print(status.user.screen_name)
+                print(tweet)
+       
 if __name__ == '__main__':
     a = MyStreamListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, a)
-    stream.filter(follow=[''])#put twitter user id here
+    stream.filter(follow=['25073877'])#put twitter user id here
+
